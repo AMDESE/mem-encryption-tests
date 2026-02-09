361bf41092aec06d7f6e7be239a62bc5a5ff2c66
Tom Lendacky <thomas.lendacky@amd.com>
2026-02-09 09:28:43 -0600

    Node allocation support
    Provide a kernel parameter that allows to specify the node on which page
    allocation is to be performed. After allocation, validate that the page
    came from the specified node.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- tsme-test.c
+++ tsme-test.c
@@ -18,11 +18,20 @@
 
 #include <asm/msr-index.h>
 #include <asm/tlbflush.h>
+#include <asm/set_memory.h>
 #include <asm/special_insns.h>
 
 MODULE_AUTHOR("Advanced Micro Devices, Inc.");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("TSME test module");
+
+static int node = NUMA_NO_NODE;
+module_param(node, int, 0444);
+MODULE_PARM_DESC(node, " allocate memory from the specified node (default is no specific node)");
+
+static unsigned int set_memory;
+module_param(set_memory, uint, 0444);
+MODULE_PARM_DESC(set_memory, " attempt to use set_memory_{uc,wb} when a page is mapped at 1G");
 
 static unsigned int debug;
 module_param(debug, uint, 0444);
@@ -44,7 +53,23 @@ static void __flush_all(void *arg)
 {
 	unsigned long flush_cache = (bool)arg;
 
-	__flush_tlb_all();
+	if (cpu_feature_enabled(X86_FEATURE_INVPCID)) {
+		struct { u64 d[2]; } desc = { { 0, 0 } };
+		unsigned long type = 2;
+
+		asm volatile("invpcid %[desc], %[type]" : : [desc] "m" (desc), [type] "r" (type) : "memory");
+	} else {
+		unsigned long cr4, flags;
+
+		raw_local_irq_save(flags);
+
+		asm volatile("mov %%cr4,%0" : "=r" (cr4));
+		asm volatile("mov %0,%%cr4" : : "r" (cr4 ^ X86_CR4_PGE) : "memory");
+		asm volatile("mov %0,%%cr4" : : "r" (cr4)               : "memory");
+
+		raw_local_irq_restore(flags);
+	}
+
 	if (flush_cache)
 		wbinvd();
 }
@@ -106,22 +131,18 @@ static int __init tsme_check_prereqs(u64 *syscfg)
 	return 0;
 }
 
-#define RETRY_COUNT 64
 static int __init tsme_test_init(void)
 {
-	unsigned long sme_mask;
-	void *buffer, *buffer_reference, *buffers[RETRY_COUNT];
-	struct page *page, *page_reference;
+	void *buffer, *buffer_reference;
 	pte_t *ptep, old_pte, new_pte;
-	unsigned int level, retry;
+	unsigned long sme_mask;
+	unsigned int level;
 	u64 syscfg;
 	int ret;
 
 	ret = tsme_check_prereqs(&syscfg);
 	if (ret)
 		return ret;
-
-	ret = -ENOMEM;
 
 	if (syscfg == SYSCFG_SME_SEV) {
 		sme_mask = BIT_ULL(cpuid_ebx(0x8000001f) & 0x3f);
@@ -135,38 +156,78 @@ static int __init tsme_test_init(void)
 		return -EINVAL;
 	}
 
-	retry = 0;
-retry:
-	page = alloc_page(GFP_KERNEL);
-	if (!page)
-		goto e_freehuge;
+	if (node != NUMA_NO_NODE && node >= nr_node_ids) {
+		pr_err("Specified node (%u) is higher than the maximum node on the system (%u)\n", node, nr_node_ids - 1);
+		return -EINVAL;
+	}
 
-	buffer = page_address(page);
+	if (node != NUMA_NO_NODE && !node_online(node)) {
+		pr_err("Specified node (%u) is not online\n", node);
+		return -EINVAL;
+	}
+
+	if (debug)
+		pr_notice("Buffer to be allocated on node %u\n", node);
+
+	buffer = kzalloc_node(PMD_SIZE, GFP_KERNEL, node);
+	if (!buffer) {
+		pr_err("Buffer allocation failed, unable to determine TSME status\n");
+		return -ENOMEM;
+	}
+
+	if (debug)
+		pr_notice("Buffer allocated on node %u\n", page_to_nid(virt_to_page(buffer)));
+
+	if (node != NUMA_NO_NODE && node != page_to_nid(virt_to_page(buffer))) {
+		pr_err("Buffer not allocated on target node, unable to determine TSME status\n");
+		ret = -ENOMEM;
+		goto e_free;
+	}
+
 	ptep = lookup_address((unsigned long)buffer, &level);
-	if (level != PG_LEVEL_4K) {
-		buffers[retry++] = buffer;
-		if (retry >= RETRY_COUNT) {
-			pr_err("Hugepage repeatedly allocated, unable to determine TSME status\n");
-			goto e_freehuge;
+	if (level > PG_LEVEL_2M) {
+		if (!set_memory) {
+			pr_err("Allocation level (%u) is greater than 2M, unable to determine TSME status\n", level);
+			ret = -ENOMEM;
+			goto e_free;
 		}
 
-		goto retry;
+		pr_notice("Buffer allocation level is %u, attempting to reduce using set_memory_{uc,wb}\n", level);
+
+		ret = set_memory_uc((unsigned long)buffer, PMD_SIZE / PAGE_SIZE);
+		if (ret) {
+			pr_err("Setting memory to UC failed, unable to determine TSME status: %d\n", ret);
+			goto e_free;
+		}
+
+		ret = set_memory_wb((unsigned long)buffer, PMD_SIZE / PAGE_SIZE);
+		if (ret) {
+			pr_err("Setting memory to WB failed, unable to determine TSME status: %d\n", ret);
+
+			/* Can't change pages back to WB, so leak them */
+			return ret;
+		}
+
+		ptep = lookup_address((unsigned long)buffer, &level);
+		pr_notice("Buffer allocation level is now %u\n", level);
+
+		if (level > PG_LEVEL_2M) {
+			pr_err("Allocation level (%u) is still mapped greater than 2M, unable to determine TSME status\n", level);
+			goto e_free;
+		}
 	}
-	memset(buffer, 0x00, PAGE_SIZE);
 
-	page_reference = alloc_page(GFP_KERNEL);
-	if (!page_reference)
+	buffer_reference = kzalloc(PAGE_SIZE, GFP_KERNEL);
+	if (!buffer_reference) {
+		pr_err("Reference allocation failed, unable to determine TSME status\n");
 		goto e_free;
-
-	buffer_reference = page_address(page_reference);
-	memset(buffer_reference, 0x00, PAGE_SIZE);
+	}
 
 	old_pte = *ptep;
 	new_pte = __pte(pte_val(*ptep) ^ sme_mask);
 
 	if (debug) {
-		pr_notice("%u additional attempts to allocate test capable buffer\n", retry);
-		pr_notice("Old PTE = %#018lx, New PTE = %#018lx\n", pte_val(old_pte), pte_val(new_pte));
+		pr_notice("Level=%u, Old PTE = %#018lx, New PTE = %#018lx\n", level, pte_val(old_pte), pte_val(new_pte));
 		pr_notice("Buffer (KeyMask=%#018lx)\n", pte_val(old_pte) & sme_mask);
 		print_hex_dump(KERN_DEBUG, "TSME Test: Buffer (first 64 bytes - before: ", DUMP_PREFIX_OFFSET, 16, 1, buffer, 64, 1);
 	}
@@ -203,16 +264,10 @@ retry:
 	if (ret)
 		pr_err("sysfs_create_file failed: ret=%d\n", ret);
 
-	free_page((unsigned long)buffer_reference);
+	kfree(buffer_reference);
 
 e_free:
-	free_page((unsigned long)buffer);
-
-e_freehuge:
-	while (retry) {
-		retry--;
-		free_page((unsigned long)buffers[retry]);
-	}
+	kfree(buffer);
 
 	return ret;
 }
